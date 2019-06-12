b069380d23fd1114736c18a51f432ceaebcfd1b7
Christian Ashby <git@cashby.me.uk>
2019-06-12 21:43:54 +0100

    Adding first incarnation of Smartthings integration (utterly undocumented\!)

---
+++ tesla-powerwall-gateway/tesla-powerwall-gateway.groovy
@@ -0,0 +1,109 @@
+/**
+ *  Tesla Powerwall Gateway II Local API Device Handler
+ *
+ *  Copyright 2019 Christian Ashby
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+ 
+preferences {
+}
+
+metadata {
+    definition (name: "Tesla Powerwall Gateway", namespace: "cscashby/smartthings", author: "Christian Ashby") {
+        capability "Polling"
+        capability "Refresh"
+        capability "Battery"
+        capability "Energy Meter"
+        capability "Power Meter"
+    }
+
+    simulator {
+        // TODO: define status and reply messages here
+    }
+
+    tiles { //st.Home.home2
+        valueTile("grid", "device.grid", width: 1, height: 1) {
+            state "grid", label:'${currentValue} kW', unit: "kW", icon: "st.Seasonal Winter.seasonal-winter-011"
+        }
+        valueTile("solar", "device.solar", width: 1, height: 1) {
+            state "solar", label:'${currentValue} kW', unit: "kW", icon: "st.Weather.weather14"
+        }
+        valueTile("battery", "device.battery", width: 1, height: 1) {
+            state "battery", label:'${currentValue} kW', unit: "kW", icon: "st.Home.home15"
+        }
+        standardTile("refresh", "device.refresh", inactiveLabel: false, decoration: "flat") {
+            state "default", action:"refresh.refresh", icon: "st.secondary.refresh"
+        }
+        main "solar"
+        details(["grid", "battery", "solar", "refresh"])
+    }
+}
+
+// handle commands
+def poll() {
+    log.debug "Executing 'poll'"
+    getReading()
+}
+
+def refresh() {
+    log.debug "Executing 'refresh'"
+    getReading()
+}
+
+// Get the sensor reading
+private getReading() {
+  def uri = "/api/meters/aggregates"
+  def headers = [:]
+  headers.put("HOST", getHostAddress())
+  log.debug "Headers are ${headers}"
+
+  def hubAction = new physicalgraph.device.HubAction([
+    method: "GET",
+    path: uri,
+    headers: headers],
+    null,
+    [callback: calledBackHandler]
+  )
+  log.debug hubAction
+  return sendHubCommand(hubAction) 
+}
+
+void calledBackHandler(physicalgraph.device.HubResponse hubResponse) {
+    log.debug "Entered calledBackHandler()..."
+    def result = hubResponse.json
+    log.debug "body in calledBackHandler() is: ${result}"
+	if( result ) {
+	    sendEvent(name: "grid", value: Math.round((result.load.instant_power/1000)*100) / 100)
+	    sendEvent(name: "solar", value: Math.round((result.solar.instant_power/1000)*100) / 100)
+	    sendEvent(name: "battery", value: Math.round((result.battery.instant_power/1000)*100) / 100)
+	}
+}
+
+// gets the address of the device
+private getHostAddress() {
+    def ip = getDataValue("ip")
+    def port = getDataValue("port")
+
+    if (!ip || !port) {
+        def parts = device.deviceNetworkId.split(":")
+        log.debug("network ID ${device.deviceNetworkId}")
+        if (parts.length == 2) {
+            ip = parts[0]
+            port = parts[1]
+        } else {
+            log.warn "Can't figure out ip and port for device: ${device.id}"
+        }
+    }
+
+    log.debug "Using IP: $ip and port: $port for device: ${device.id}"
+    return ip + ":" + port
+}
